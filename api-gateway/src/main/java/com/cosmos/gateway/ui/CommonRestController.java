d948f1453c4c813cce70d8082fdcd923ef9cd970
JaeHan-Kim <hgal8877@gmail.com>
2022-04-14 00:46:53 +0900

    feat: 작업중

---
+++ api-gateway/src/main/java/com/cosmos/gateway/ui/CommonRestController.java
@@ -0,0 +1,14 @@
+package com.cosmos.gateway.ui;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class CommonRestController implements ErrorController {
+
+}
---
+++ api-product/src/main/java/com/cosmos/product/domain/Money.java
@@ -0,0 +1,11 @@
+package com.cosmos.product.domain;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Money extends ValueObject<Money> {
+private BigDecimal price;
+
+
+}
